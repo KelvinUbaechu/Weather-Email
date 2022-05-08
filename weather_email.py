cdf5c283c3a091eed868f1eee0a4c2b8f3d116b5
Kelvin Ubaechu <kubaechu@gmail.com>
2022-05-08 11:17:18 -0400

    Add function for getting forecast json

--- weather_email.py
+++ weather_email.py
@@ -1,4 +1,9 @@
-from typing import TypedDict
+import os, requests
+from dotenv import load_dotenv
+from typing import Any, TypedDict
+
+
+load_dotenv()
 
 
 class Forecast(TypedDict):
@@ -8,3 +13,16 @@ class Forecast(TypedDict):
     total_precip: float
     condition: str  # Such as Partly Cloudy or Sunny
     icon_filename: str
+
+
+def get_forecast_json(zip_code: str) -> dict[str, Any]:
+    """Returns the forecast data for the specified zip code
+    
+    Currently, there are no checks to determine whether the zip code
+    is valid or if the API can be accessed"""
+    api_key = os.getenv('WEATHER_API_KEY')
+    params = {'key': api_key, 'q': zip_code, 'days': 1,
+          'aqi': 'no', 'alerts': 'no'}
+    link = 'http://api.weatherapi.com/v1/forecast.json'
+    response = requests.get(link, params=params)
+    return response.json()
