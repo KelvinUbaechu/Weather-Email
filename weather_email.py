d48ead215d47e18303fc24bfcee20fb102e4e566
Kelvin Ubaechu <kubaechu@gmail.com>
2022-05-09 16:43:18 -0400

    Add function to get Google API credentials

--- weather_email.py
+++ weather_email.py
@@ -3,6 +3,10 @@ from base64 import urlsafe_b64encode
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 from dotenv import load_dotenv
 from typing import Any, TypedDict
 
@@ -67,3 +71,24 @@ def construct_email(forecast: Forecast) -> dict[str, bytes]:
     img.add_header('Content-Disposition', 'inline', filename=forecast['icon_filename'].split('.')[0])
     message.attach(img)
     return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
+
+def get_credentials() -> Credentials | None:
+    scopes = ['https://www.googleapis.com/auth/gmail.modify']
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', scopes)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', scopes)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    return creds
